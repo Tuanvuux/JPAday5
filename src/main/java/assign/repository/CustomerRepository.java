419aed4f7cf1174f59c14add2c945585bfdf4d65
Tuanvuux <nguyentuanvu1410@gmail.com>
2023-12-22 15:51:45 +0700

    commit excise

---
+++ src/main/java/assign/repository/CustomerRepository.java
@@ -0,0 +1,25 @@
+package assign.repository;
+
+import entity.Customer;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
+
+import javax.naming.Name;
+import java.util.List;
+import java.util.Optional;
+
+public interface CustomerRepository extends CrudRepository<Customer, Integer> {
+    List<Customer> findAll();
+    Optional<Customer> findById(Integer Id);
+
+    List<Customer> findByName(String name);
+
+    List<Customer> findByNameOrEmail(String name, String email);
+
+    @Query("SELECT c FROM Customer c WHERE c.sex = 'Nam' AND YEAR(CURRENT_DATE) - YEAR(c.birthdate) BETWEEN 20 AND 30")
+    List<Customer> findMaleCustomersBetween20And30();
+
+
+
+
+}
